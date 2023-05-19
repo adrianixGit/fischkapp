78e4af32e690e03522338548146b164a6c4d8031
Adrian <adrian.gorniak97@op.pl>
2023-05-19 19:20:50 +0200

    deleted some unnecessary files

--- fischkapp/js/data/createState.js
+++
@@ -1,5 +0,0 @@
-export const createState = () => {
-  return {
-    flashcards: [],
-  };
-};
--- fischkapp/src/App.tsx
+++ fischkapp/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "./App.css";
-
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/theme/theme";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
