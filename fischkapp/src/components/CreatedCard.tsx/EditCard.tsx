3e9bd9f0e7d1a936d1b54c0fabf567219a67e2cc
Adrian <adrian.gorniak97@op.pl>
2023-06-15 17:17:33 +0200

    Open and cancel editing

--- fischkapp/src/components/CreatedCard.tsx/EditCard.tsx
+++ fischkapp/src/components/CreatedCard.tsx/EditCard.tsx
@@ -1,9 +1,9 @@
 import { StyledCardContent, StyledIcon } from "../styles/CardGlobal.styled";
-
 import { StyledInput } from "../styles/AddNewCard.styled";
 import { StyledCardButton } from "../styles/buttons/CardButton";
 import { StyledButtonsContainer } from "../styles/AddNewCard.styled";
 import deleteIcon from "../../assets/deleteIcon.svg";
+import { useState } from "react";
 
 type EditCardPropType = {
   onEdit: Function;
@@ -11,12 +11,18 @@ type EditCardPropType = {
 };
 
 export const EditCard = ({ onEdit, cardText }: EditCardPropType) => {
+  const [newCardText, setNewCardText] = useState("");
+
   return (
     <StyledCardContent>
       <StyledIcon>
         <img src={deleteIcon} alt="editIcon" />
       </StyledIcon>
-      <StyledInput type="text" placeholder={cardText} />
+      <StyledInput
+        type="text"
+        placeholder={cardText}
+        onChange={(e) => setNewCardText(e.target.value)}
+      />
       <StyledButtonsContainer>
         <StyledCardButton onClick={() => onEdit()}>Cancel</StyledCardButton>
         <StyledCardButton bg="violet">Save</StyledCardButton>
