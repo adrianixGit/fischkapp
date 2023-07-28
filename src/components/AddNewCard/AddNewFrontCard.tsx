9ea6a42cf1157453f922433757818dc0d30ceb13
Adrian <adrian.gorniak97@op.pl>
2023-07-28 19:08:26 +0200

    text stays after moving on and coming back

--- src/components/AddNewCard/AddNewFrontCard.tsx
+++ src/components/AddNewCard/AddNewFrontCard.tsx
@@ -7,18 +7,21 @@ type FrontCardProps = {
   onFlipCard: () => void;
   onSetNewCard: (isSettingCard: boolean) => void;
   onSetFrontText: (frontText: string) => void;
+  frontText: string;
 };
 
 export const AddNewFrontCard = ({
   onFlipCard,
   onSetNewCard,
   onSetFrontText,
+  frontText,
 }: FrontCardProps) => {
   return (
     <StyledCardContent>
       <StyledInput
         type="text"
         placeholder="Type word.."
+        value={frontText}
         onChange={(e) => onSetFrontText(e.target.value)}
       />
       <StyledButtonsContainer>
